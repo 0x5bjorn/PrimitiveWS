4c14286dc89b79dfc472c785b36ace6b63811363
Sulley <name.sul27@gmail.com>
2022-07-17 16:37:37 +0600

    Code refactoring

--- primitive_ws.py
+++ primitive_ws.py
@@ -1,28 +1,39 @@
 import socket
 
 # Define host and port
-SERVER_HOST, SERVER_PORT = '', 8888
+SERVER_ADDRESS = (SERVER_HOST, SERVER_PORT) = ('', 8888)
+# Define connection number of unaccepted connections
+# that the system will allow before refusing new connections
+# and maximum amount of data to be received at once
+CONN_NUMBER, RECV_BUFFER = 3, 1024
 
-# Create and set up listening socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind((SERVER_HOST, SERVER_PORT))
-server_socket.listen(1)
+def init_server():
+    # Create and set up listening socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind(SERVER_ADDRESS)
+    server_socket.listen(CONN_NUMBER)
 
-print(f'Serving/listening HTTP on port {SERVER_PORT} ...')
-
-while True:
-    # Wait for client connections
-    client_connection, client_addr = server_socket.accept()
-
+def handle_request():
     # Get the client request
-    request = client_connection.recv(1024).decode('utf-8')
+    request = client_connection.recv(RECV_BUFFER).decode('utf-8')
     print(request)
 
     # Send HTTP response
     response = "Hello, World!\n"
     client_connection.sendall(response.encode())
-    client_connection.close()
 
-# Close socket
-server_socket.close()
+def serve():
+    init_server()
+
+    print(f'Serving/listening on port {SERVER_PORT} ...')
+    while True:
+        # Wait for client connections
+        client_connection, client_addr = server_socket.accept()
+        handle_request()
+        client_connection.close()
+
+if __name__ == '__main__':
+    serve()
+    # Close socket
+    server_socket.close()
