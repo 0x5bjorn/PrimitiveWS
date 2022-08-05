bbe15a73a15479acd6ce9bc1ea6ff59f810dfd70
Sulley <name.sul27@gmail.com>
2022-08-05 16:56:01 +0600

    Minor fixes and resolved #6

--- primitive_server.py
+++ primitive_server.py
@@ -3,6 +3,8 @@ import time
 
 class PrimServer:
 
+    is_stopped = False
+
     # Type of addresses (IPv4 addresses)
     address_family = socket.AF_INET
     # TCP connection
@@ -14,32 +16,41 @@ class PrimServer:
     # Maximum amount of data to be received at once
     recv_buffer = 1024
 
-    def __init__(self, server_address):
+    def __init__(self, server_address=None):
         # Create and set up listening socket
         self.server_socket = socket.socket(
             self.address_family,
             self.socket_type
         )
         self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        if server_address:
+            self.server_socket.bind(server_address)
+            self.server_socket.listen(self.conn_number)
+
+    def setup(self, server_address):
+        # Set up listening socket
         self.server_socket.bind(server_address)
         self.server_socket.listen(self.conn_number)
 
     def __del__(self):
         # Close socket after finishing using webserver
-        if self.server_socket:
+        if self.server_socket.fileno() != -1:
+            self.is_stopped = True
             print("\n" + time.asctime(), f"- Server stops on {self.server_socket.getsockname()}")
             self.server_socket.close()
 
     def shutdown(self):
         # Close socket after finishing using webserver
-        if self.server_socket:
+        if self.server_socket.fileno() != -1:
+            self.is_stopped = True
             print("\n" + time.asctime(), f"- Server stops on {self.server_socket.getsockname()}")
             self.server_socket.close()
 
-    def serve(self):
+    def run(self):
         print(time.asctime(), f"- Serving/listening on {self.server_socket.getsockname()}...")
-        while True:
+        while not self.is_stopped:
             # Wait for client connections
+            print(self.server_socket.fileno())
             self.client_connection, client_addr = self.server_socket.accept()
             self.handle_request()
             self.client_connection.close()
